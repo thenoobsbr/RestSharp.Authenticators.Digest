947b46b91880d7de7682811b07702e6b1aa04473
Bernardo Esbérard <bernardbr@gmail.com>
2021-12-21 12:28:22 -0300

    :white_check_mark: Implements some tests.

--- test/unitario/DigestTest.cs
+++ test/unitario/DigestTest.cs
@@ -1,38 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
 
-namespace RestSharp.Authenticators.Digest.Tests
+namespace RestSharp.Authenticators.Digest.Tests;
+
+/// <summary>
+///     The integration tests for <see cref="DigestAuthenticator" />.
+/// </summary>
+[Trait("Category", "IntegrationTests")]
+[Trait("Class", nameof(DigestAuthenticator))]
+public class DigestTest
 {
-    /// <summary>
-    /// The integration tests for <see cref="DigestAuthenticator"/>.
-    /// </summary>
-    [Trait("Category", "IntegrationTests")]
-    [Trait("Class", nameof(DigestAuthenticator))]
-    public class DigestTest
+    [SkippableFact]
+    public void Given_ADigestAuthEndpoint_When_ITryToGetInfo_Then_TheAuthMustBeResolved()
     {
-        /// <summary>
-        /// Given an digest auth endpoint, When I try to get info, Then the auth must be resolved.
-        /// </summary>
-        [Fact]
-        public void Given_AnDigestAuthEndpoint_When_ITryToGetInfo_Then_TheAuthMustBeResolved()
+        Skip.IfNot(Debugger.IsAttached);
+
+        var client = new RestClient("http://localhost:49896/api")
         {
-            var client = new RestClient("http://localhost:49896/api")
-            {
-                Authenticator = new DigestAuthenticator("eddie", "starwars123")
-            };
+            Authenticator = new DigestAuthenticator("eddie", "starwars123")
+        };
 
-            var request = new RestRequest("values", Method.GET);
-            request.AddHeader("Content-Type", "application/json");
+        var request = new RestRequest("values", Method.GET);
+        request.AddHeader("Content-Type", "application/json");
 
-            var response = client.Execute(request);
+        var response = client.Execute(request);
 
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
+    [InlineData(
+        "Digest realm=\"test - realm\", nonce=\"2021 - 12 - 21 13:40:54.513311Z f152e55bf3d14e28b90f47db5dbd8afb\", qop=\"auth\", algorithm=MD5")]
+    [InlineData(
+        "realm=\"test - realm\", nonce=\"2021 - 12 - 21 13:40:54.513311Z f152e55bf3d14e28b90f47db5dbd8afb\", qop=\"auth\", algorithm=MD5")]
+    public void Given_ADigestAuthenticateHeader_When_ITryCreateObject_Then_AllPropsMustBeFilled(string header)
+    {
+        var digestHeader = new DigestHeader(header);
+        digestHeader.Nonce.Should().Be("2021 - 12 - 21 13:40:54.513311Z f152e55bf3d14e28b90f47db5dbd8afb");
+        digestHeader.Qop.Should().Be("auth");
+        digestHeader.Realm.Should().Be("test - realm");
     }
 }
